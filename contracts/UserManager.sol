332fe62f5c1d7aa023f5beec85b3f7aa767e9dae
Nathan Soh <nathansoh@gmail.com>
2023-12-02 18:56:48 +0800

    add user contracts

---
+++ contracts/UserManager.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "./UserProfile.sol";
+
+contract UserManager {
+    mapping(address => address) public users;
+
+    event UserRegistered(address indexed user, address profile);
+
+    function registerUser() public {
+        require(users[msg.sender] != address(0), "User already registered");
+        
+        UserProfile newUser = new UserProfile(msg.sender);
+        users[msg.sender] = address(newUser);
+
+        emit UserRegistered(msg.sender, address(newUser));
+    }
+
+    function getUserProfile() public view returns (address) {
+        return users[msg.sender];
+    }
+}
---
+++ contracts/UserProfile.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+contract UserProfile {
+    address public walletAddress;
+    string public profileImageIPFSHash;
+    string public profileHeaderIPFSHash;
+    string public profileDataIPFSHash;
+
+    address[] public connections;
+    address[] public pendingConnections;
+
+    event ConnectionRequestReceived(address indexed _from);
+
+    modifier onlyUser(address _walletAddress) {
+        require(msg.sender == _walletAddress, "Only the user of this profile can perform this action!");
+        _;
+    }
+
+    constructor (address _walletAddress) {
+        walletAddress = _walletAddress;
+    }
+
+    function setProfileImageHash(string memory _hash) public {
+        profileImageIPFSHash = _hash;
+    }
+
+    function setProfileHeaderHash(string memory _hash) public {
+        profileHeaderIPFSHash = _hash;
+    }
+
+    function setProfileDataHash(string memory _hash) public {
+        profileDataIPFSHash = _hash;
+    }
+
+    function acceptConnection(address _connectionAddress) public onlyUser(walletAddress) {
+        connections.push(_connectionAddress);
+        uint index = getIndexOfPendingConnection(_connectionAddress);
+        removeConnectionFromPendingConnections(index);
+
+    }
+
+    function addRequest(address _connectionAddress) public {
+        pendingConnections.push(_connectionAddress);
+        emit ConnectionRequestReceived(_connectionAddress);
+    }
+
+    function getIndexOfPendingConnection(address _connectionAddress) internal view returns (uint) {
+        for (uint i = 0; i < pendingConnections.length; i++) {
+            if (pendingConnections[i] == _connectionAddress) {
+                return i;
+            }
+        }
+        revert('This connection is not in the pending connections list');
+    }
+
+    function removeConnectionFromConnections(uint _index) public onlyUser(walletAddress) {
+        if (_index >= connections.length) return;
+
+        for (uint i = _index; i < connections.length - 1; i++) {
+            connections[i] = connections[i+1];
+        }
+        connections.pop();
+    }
+
+    function removeConnectionFromPendingConnections(uint _index) public onlyUser(walletAddress) {
+        if (_index >= pendingConnections.length) return;
+
+        for (uint i = _index; i < pendingConnections.length - 1; i++) {
+            pendingConnections[i] = pendingConnections[i+1];
+        }
+        pendingConnections.pop();
+    }
+}

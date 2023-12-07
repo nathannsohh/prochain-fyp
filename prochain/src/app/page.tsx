6f06e05f23677cefbe1bcbbbf1be6738845931b1
Nathan Soh <nathansoh@gmail.com>
2023-12-07 17:23:17 +0800

    remove unneeded code

--- prochain/src/app/page.tsx
+++ prochain/src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image'
 import styles from './page.module.css'
-import Wallet from '../components/Wallet'
 import { useMetamask } from '../hooks/useMetamask';
 import { useListen } from '@/hooks/useListen';
 import { useEffect } from 'react';
@@ -50,28 +49,28 @@ export default function Home() {
 
 
 
-const showInstallMetamask = status !== "pageNotLoaded" && !isMetamaskInstalled;
-const showConnectButton = status !== "pageNotLoaded" && isMetamaskInstalled && !wallet;
+// const showInstallMetamask = status !== "pageNotLoaded" && !isMetamaskInstalled;
+// const showConnectButton = status !== "pageNotLoaded" && isMetamaskInstalled && !wallet;
 
-const isConnected = status !== "pageNotLoaded" && typeof wallet === "string";
+// const isConnected = status !== "pageNotLoaded" && typeof wallet === "string";
 
-const handleConnect = async () => {
-    dispatch({ type: "loading" });
-    const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-    });
+// const handleConnect = async () => {
+//     dispatch({ type: "loading" });
+//     const accounts = await window.ethereum.request({
+//         method: "eth_requestAccounts",
+//     });
 
-    if (accounts.length > 0) {
-        const balance = await window.ethereum!.request({
-            method: "eth_getBalance",
-            params: [accounts[0], "latest"],
-        });
-        dispatch({ type: "connect", wallet: accounts[0], balance });
+//     if (accounts.length > 0) {
+//         const balance = await window.ethereum!.request({
+//             method: "eth_getBalance",
+//             params: [accounts[0], "latest"],
+//         });
+//         dispatch({ type: "connect", wallet: accounts[0], balance });
 
-        // we can register an event listener for changes to the users wallet
-        listen();
-    }
-}
+//         // we can register an event listener for changes to the users wallet
+//         listen();
+//     }
+// }
 
 const handleDisconnect = () => {
     dispatch({ type: "disconnect" });

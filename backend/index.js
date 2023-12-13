f856e2ca2049dd014f94b26a8eea25d0ce640cb7
Nathan Soh <nathansoh@gmail.com>
2023-12-13 17:45:24 +0800

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const User = require('./routes/user.js')
 
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const port = 8000
 

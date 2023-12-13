0601a993239e0c9ec8ab98d436f544007b906fa4
Nathan Soh <nathansoh@gmail.com>
2023-12-13 18:35:27 +0800

    add delete endpoint for users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,7 @@ const db = require('../db/index')
 exports.userRoutes = (app) => {
     app.post('/user', async (req, res) => {
         let response;
+
         try {
             const query = {
                 text: 'INSERT INTO users (first_name, last_name, email, pronouns, bio, wallet_address) VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
@@ -11,6 +12,28 @@ exports.userRoutes = (app) => {
             response = {
                 success: true,
                 hash: result.rows[0].id
+            }
+            res.status(200).send(response)
+        } catch (e) {
+            response = {
+                success: false,
+                message: e.message
+            }
+            res.status(400).send(response)
+        }
+    })
+
+    app.delete('/user/:id', async (req, res) => {
+        let response;
+        try {
+            const query = {
+                text: "DELETE FROM users WHERE id = $1",
+                values: [req.params.id]
+            }
+            await db.query(query)
+            response = {
+                success: true,
+                message: `User of hash ${req.params.id} deleted!`
             }
             res.status(200).send(response)
         } catch (e) {

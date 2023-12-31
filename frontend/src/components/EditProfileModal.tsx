5f1223a623f26bba69c0987a821820f64a49af9b
Nathan Soh <nathansoh@gmail.com>
2023-12-31 19:25:50 +0800

    add edit profile modal and integrate with endpoint

---
+++ frontend/src/components/EditProfileModal.tsx
@@ -0,0 +1,137 @@
+import { Modal, ModalOverlay, ModalContent, ModalBody, ModalHeader, ModalFooter, Button, ModalCloseButton, Textarea, Avatar, HStack, Text, FormControl, FormErrorMessage, Input, FormLabel, Select, Spacer } from "@chakra-ui/react";
+import { useEffect } from "react";
+import axios from "axios";
+import { useForm } from 'react-hook-form'
+import { UserType } from "@/util/types";
+import { countryList } from "@/util/constants";
+
+
+interface EditProfileModalProps {
+    isOpen: boolean,
+    onClose: () => void,
+    triggerToast: (title: string, description: string, status: "loading" | "info" | "warning" | "success" | "error" | undefined) => void
+    userData: UserType | null
+}
+
+export default function EditProfileModal(props: EditProfileModalProps) {
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting },
+    } = useForm<UserType>()
+
+    useEffect(() => {
+        console.log(props.userData)
+    }, [])
+
+    const submitHandler = async (values: UserType) => {
+        try {
+            console.log(values);
+            const response = await axios.put('http://localhost:8000/user', {...values, wallet_address: props.userData?.wallet_address})
+            if (response.data.success) {
+                props.onClose()
+                props.triggerToast("Success", "Your profile has been updated.", "success")
+            }
+        } catch (e) {
+            props.triggerToast("Error", "Something went wrong and your profile was not updated.", "error")
+        }
+    }
+
+    return(
+        <Modal isOpen={props.isOpen} onClose={props.onClose}>
+            <ModalOverlay />
+            <ModalContent maxW="50%">
+                <ModalHeader>Edit Profile Card</ModalHeader>
+                <ModalCloseButton />
+                <form onSubmit={handleSubmit(submitHandler)}>
+                    <ModalBody>
+                        <HStack width="100%" mb={3}>
+                            <FormControl isRequired isInvalid={errors.first_name != null}>
+                                <FormLabel>First Name</FormLabel>
+                                <Input 
+                                placeholder='First Name' 
+                                defaultValue={props.userData?.first_name}
+                                {...register('first_name', {
+                                    required: 'This is required'
+                                })}
+                                />
+                                <FormErrorMessage>
+                                {errors.first_name && errors.first_name.message}
+                                </FormErrorMessage>
+                            </FormControl>
+                            <FormControl isRequired isInvalid={errors.last_name != null}>
+                                <FormLabel>Last Name</FormLabel>
+                                <Input 
+                                placeholder='Last Name'
+                                defaultValue={props.userData?.last_name}
+                                {...register('last_name', {
+                                    required: 'This is required'
+                                })}
+                                />
+                                <FormErrorMessage>
+                                {errors.last_name && errors.last_name.message}
+                                </FormErrorMessage>
+                            </FormControl>
+                        </HStack>
+                        <HStack width="100%" mb={3}>
+                            <FormControl width="20%">
+                                <FormLabel>Pronouns</FormLabel>
+                                <Select 
+                                placeholder='Pronouns'
+                                defaultValue={props.userData?.pronouns === null ? undefined : props.userData!.pronouns!}
+                                {...register('pronouns')}
+                                >
+                                    <option value='He/Him'>He/Him</option>
+                                    <option value='He/Him'>She/Her</option>
+                                    <option value='He/Him'>They/Them</option>
+                                    <option value='NA'>Not Applicable</option>
+                                </Select>
+                            </FormControl>
+                            <FormControl width="20%">
+                                <FormLabel>Location</FormLabel>
+                                <Select
+                                placeholder='Location'
+                                defaultValue={props.userData?.location === null ? undefined : props.userData!.location!}
+                                {...register('location')}
+                                >
+                                    {countryList.map((country: string, index: number) => {
+                                        return <option key={index} value={country}>{country}</option>
+                                    })}
+                                </Select>
+                            </FormControl>
+                        </HStack>
+                        <FormControl isRequired isInvalid={errors.email != null} width="60%" mb={3}>
+                            <FormLabel>Email</FormLabel>
+                            <Input 
+                            placeholder='Email'
+                            defaultValue={props.userData?.email}
+                            {...register('email', {
+                                required: 'This is required',
+                                pattern: {
+                                    value: /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/,
+                                    message: "Please key in a valid email address!"
+                                }
+                            })}
+                            />
+                            <FormErrorMessage>
+                            {errors.email && errors.email.message}
+                            </FormErrorMessage>
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Bio</FormLabel>
+                            <Textarea 
+                            placeholder='Bio' 
+                            defaultValue={props.userData?.bio === null ? undefined : props.userData!.bio!}
+                            resize="none"
+                            {...register('bio')}
+                            />
+                        </FormControl>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme="blue" type="submit" isLoading={isSubmitting}>Save</Button>
+                    </ModalFooter>
+                </form>
+            </ModalContent>
+        </Modal>
+    )    
+}

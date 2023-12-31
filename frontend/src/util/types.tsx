f9c8741f88070bae92ad6ec4d3145ff282839320
Nathan Soh <nathansoh@gmail.com>
2023-12-31 19:24:54 +0800

    add common type for app

---
+++ frontend/src/util/types.tsx
@@ -0,0 +1,9 @@
+export interface UserType {
+    first_name: string,
+    last_name: string,
+    pronouns: string | null,
+    email: string,
+    wallet_address: string,
+    bio: string | null,
+    location: string | null
+}

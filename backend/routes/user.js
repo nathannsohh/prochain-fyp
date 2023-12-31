0011811d4e428c5e1a4630107649cf0acad1c923
Nathan Soh <nathansoh@gmail.com>
2023-12-31 19:22:53 +0800

    add user update endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -67,4 +67,27 @@ exports.userRoutes = (app) => {
             res.status(400).send(response)
         }
     })
+
+    app.put('/user', async (req, res) => {
+        let response;
+        try {
+            const query = {
+                text: "UPDATE users SET first_name = $1, last_name = $2, email = $3, pronouns = $4, bio = $5, location = $6 WHERE wallet_address = $7 RETURNING id",
+                values: [req.body.first_name, req.body.last_name, req.body.email, req.body.pronouns, req.body.bio, req.body.location, req.body.wallet_address] 
+            }
+            const result = await db.query(query)
+            console.log(result)
+            response = {
+                success: true,
+                message: `User of hash ${result.rows[0].id} updated!`
+            }
+            res.status(200).send(response)
+        } catch (e) {
+            response = {
+                success: false,
+                message: e.message
+            }
+            res.status(400).send(response)
+        }
+    })
 }

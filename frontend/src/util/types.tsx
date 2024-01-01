c3c6151100a088929c5d56aa8c753ffe7ade01c9
Nathan Soh <nathansoh@gmail.com>
2024-01-01 21:12:26 +0800

    remove unnecessary types file

--- frontend/src/util/types.tsx
+++
@@ -1,9 +0,0 @@
-export interface UserType {
-    first_name: string,
-    last_name: string,
-    pronouns: string | null,
-    email: string,
-    wallet_address: string,
-    bio: string | null,
-    location: string | null
-}

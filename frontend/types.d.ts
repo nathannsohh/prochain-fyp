77e6699a21e911fbc1bd0a9790a83abf29d54b64
Nathan Soh <nathansoh@gmail.com>
2024-01-01 21:14:20 +0800

    define user type and user state type for redux

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,12 +1,37 @@
 type InjectedProviders = {
-    isMetaMask?: true;
-  };
+  isMetaMask?: true;
+};
   
-  interface Window {
-    ethereum: InjectedProviders & {
-      on: (...args: any[]) => void;
-      removeListener?: (...args: any[]) => void;
-      removeAllListeners: (...args: any[]) => void;
-      request<T = any>(args: any): Promise<T>;
-    };
-  }
+interface Window {
+  ethereum: InjectedProviders & {
+    on: (...args: any[]) => void;
+    removeListener?: (...args: any[]) => void;
+    removeAllListeners: (...args: any[]) => void;
+    request<T = any>(args: any): Promise<T>;
+  };
+}
+
+interface UserType {
+  first_name: string,
+  last_name: string,
+  pronouns: string | null,
+  email: string,
+  wallet_address: string,
+  bio: string | null,
+  location: string | null
+}
+
+interface ProfileState {
+  value: UserStateType | null
+}
+
+interface UserStateType  {
+  first_name: string,
+  last_name: string,
+  pronouns: string | null,
+  email: string,
+  wallet_address: string,
+  bio: string | null,
+  location: string | null,
+  connections: Number
+}

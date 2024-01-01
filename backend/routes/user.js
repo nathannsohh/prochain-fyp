579fa772370926d90b726c93ecc85ff28ce5b562
Nathan Soh <nathansoh@gmail.com>
2024-01-01 21:10:01 +0800

    add location in add user endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,8 +5,8 @@ exports.userRoutes = (app) => {
 
         try {
             const query = {
-                text: 'INSERT INTO users (first_name, last_name, email, pronouns, bio, wallet_address) VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
-                values: [req.body.firstName, req.body.lastName, req.body.email, req.body.pronouns, req.body.bio, req.body.walletAddress]
+                text: 'INSERT INTO users (first_name, last_name, email, pronouns, bio, location, wallet_address) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+                values: [req.body.first_name, req.body.last_name, req.body.email, req.body.pronouns, req.body.bio, req.body.location, req.body.walletAddress]
             }
             const result = await db.query(query)
             response = {
@@ -76,7 +76,6 @@ exports.userRoutes = (app) => {
                 values: [req.body.first_name, req.body.last_name, req.body.email, req.body.pronouns, req.body.bio, req.body.location, req.body.wallet_address] 
             }
             const result = await db.query(query)
-            console.log(result)
             response = {
                 success: true,
                 message: `User of hash ${result.rows[0].id} updated!`

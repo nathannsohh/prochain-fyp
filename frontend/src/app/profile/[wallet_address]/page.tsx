f86483b6a08f1924206acbdd8b32fd40aac9a587
Nathan Soh <nathansoh@gmail.com>
2024-01-03 18:08:43 +0800

    update data retrieval from user factory and reformat code

--- frontend/src/app/profile/[wallet_address]/page.tsx
+++ frontend/src/app/profile/[wallet_address]/page.tsx
@@ -3,23 +3,22 @@ import ProfileHead from "@/components/profile/ProfileHead";
 import ProfileNewPostModal from "@/components/profile/ProfileNewPostModal";
 import ProfilePostCard from "@/components/profile/ProfilePostCard";
 import { useMetamask } from "@/hooks/useMetamask";
-import useUserManangerContract from "@/hooks/useUserManagerContract";
+import useUserFactoryContract from "@/hooks/useUserFactoryContract";
 import { Box, useDisclosure, useToast } from "@chakra-ui/react"
 import axios from "axios";
-import { Contract, ethers } from "ethers";
+import { Contract } from "ethers";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import ABI from "@/../../artifacts/contracts/UserProfile.sol/UserProfile.json";
 import EditProfileModal from "@/components/profile/EditProfileModal";
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import { updateSelf } from "../profileSlice";
 
 
 export default function ProfilePage({ params }: { params: { wallet_address: string } }) {
-    const { state: { wallet, status, signer } } = useMetamask();
+    const { state: { wallet, status } } = useMetamask();
     const ownProfile = useAppSelector((state) => state.ownProfile.value)
     const router = useRouter()
-    const userManagerContract: Contract | null = useUserManangerContract();
+    const userFactoryContract: Contract | null = useUserFactoryContract();
     const toast = useToast()
     const [userData, setUserData] = useState<UserType | null>(params.wallet_address === wallet ? ownProfile : null)
     const [connections, setConnections] = useState<Number | null>(params.wallet_address === wallet ? ownProfile?.connections! : null)
@@ -30,11 +29,10 @@ export default function ProfilePage({ params }: { params: { wallet_address: stri
     const { isOpen: editProfileModalIsOpen, onOpen:editProfileModalOnOpen, onClose: editProfileModalOnClose } = useDisclosure();
 
     useEffect(() => {
-        console.log(params)
         if (status === "idle") {
             if (wallet === null) router.push('/login')
-            console.log(userManagerContract);
-            userManagerContract?.doesUserExist().then((result) => {
+            
+            userFactoryContract?.doesUserExist(wallet).then((result) => {
                 if (!result) {
                     router.push('/profile/new')
                 } else {
@@ -48,10 +46,8 @@ export default function ProfilePage({ params }: { params: { wallet_address: stri
 
     const getUserDetails = async (wallet_address: string | null) => {
         try {
-            const userAddress: string = await userManagerContract?.getUserProfile(wallet_address)
-            const userProfileContract = new ethers.Contract(userAddress, ABI.abi, signer)
-            const [profileDataHash, numOfConnections]  = await Promise.all([userProfileContract.profileDataHash(), userProfileContract.getNumberOfConnections()])
-            const result = await axios.get(`http://localhost:8000/user/${profileDataHash}`)
+            const [user, numOfConnections] = await Promise.all([userFactoryContract?.getUserProfile(wallet_address), userFactoryContract?.getNumberOfConnections(wallet_address)])
+            const result = await axios.get(`http://localhost:8000/user/${user.profileDataHash}`)
             if (result.data.success) {
                 const user: UserType = result.data.user
                 const userDetails = {
@@ -92,8 +88,20 @@ export default function ProfilePage({ params }: { params: { wallet_address: stri
         <Box bg="#F6F6F6">
             <ProfileHead userData={userData} onEditProfile={editProfileModalOnOpen} connections={connections}/>
             <ProfilePostCard posts={[]} ownProfile={true} onNewPost={newPostModalOnOpen}/>
-            {newPostModalIsOpen && <ProfileNewPostModal isOpen={newPostModalIsOpen} onClose={newPostModalOnClose} profileName={userData?.first_name + ' ' + userData?.last_name} triggerToast={triggerToast}/>}
-            {editProfileModalIsOpen && <EditProfileModal isOpen={editProfileModalIsOpen} onClose={editProfileModalOnClose} triggerToast={triggerToast} userData={userData} updateUserData={updateUserData}/>}
+            {newPostModalIsOpen && 
+                <ProfileNewPostModal 
+                    isOpen={newPostModalIsOpen} 
+                    onClose={newPostModalOnClose} 
+                    profileName={userData?.first_name + ' ' + userData?.last_name} 
+                    triggerToast={triggerToast}/>}
+
+            {editProfileModalIsOpen && 
+                <EditProfileModal 
+                    isOpen={editProfileModalIsOpen} 
+                    onClose={editProfileModalOnClose} 
+                    triggerToast={triggerToast} 
+                    userData={userData} 
+                    updateUserData={updateUserData}/>}
         </Box>
     )
 }

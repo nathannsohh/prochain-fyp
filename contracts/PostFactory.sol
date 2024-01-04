f4d5353ba8793eaaa461789c69f4a282d3ab919f
Nathan Soh <nathansoh@gmail.com>
2024-01-04 12:17:33 +0800

    overhaul posts and comments to structs to reduce gas fees

--- contracts/PostFactory.sol
+++ contracts/PostFactory.sol
@@ -1,19 +1,86 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "./Post.sol";
+import "./libraries/PostLibrary.sol";
+import "./ProCoinToken.sol";
 
 contract PostFactory {
+
+    mapping(uint => PostLibrary.Post) posts;
+    uint postCount;
     
-    event PostCreated(address indexed _postAddress);
-    address procoinAddress;
+    ProCoinToken procoin;
+
+    event PostCreated(
+        uint _id,
+        string _postImageHash,
+        string _postContentHash,
+        address _owner
+    );
+    event CommentCreated(uint indexed _commentId);
+    event PostLiked(uint indexed _postId);
 
     constructor(address _procoinAddress) {
-        procoinAddress = _procoinAddress;
+        procoin = ProCoinToken(_procoinAddress);
+        postCount = 0;
     }
 
     function createPost(string memory _imageHash, string memory _contentHash) public {
-        Post newPost = new Post(_imageHash, _contentHash, msg.sender, procoinAddress);
-        emit PostCreated(address(newPost));
+        address[] memory likedBy;
+        postCount++;
+        PostLibrary.Post storage newPost = posts[postCount];
+
+        newPost.postImageHash = _imageHash;
+        newPost.postContentHash = _contentHash;
+        newPost.id = postCount;
+        newPost.likedBy = likedBy;
+        newPost.likeMilestone = 0;
+        newPost.comments.push();
+        newPost.commentMilestone = 0;
+        newPost.commentCount = 0;
+        newPost.owner = msg.sender;
+
+        emit PostCreated(postCount, _imageHash, _contentHash, msg.sender);
+    }
+
+    // ###################### POST FUNCTIONS ###########################
+    function likePost(uint _id) public {
+        PostLibrary.likePost(posts[_id]);
+
+        if (posts[_id].likedBy.length >= 1
+        // TODO: Uncomment after testing the automatic distributeReward function 
+        // && posts[_id].likedBy.length / 10 > posts[_id].likeMilestone
+        ) {
+            // Make sure that users cannot like and unlike to get reward
+            posts[_id].likeMilestone = posts[_id].likedBy.length / 10;
+
+            // Transfer tokens to user
+            distributeReward(posts[_id].owner);
+        }
+        emit PostLiked(_id);
+    }
+
+    function unlikePost(uint _id) public {
+        PostLibrary.unlikePost(posts[_id]);
+    }
+
+    function comment(uint _id, string memory _commentHash) public {
+        PostLibrary.comment(posts[_id], _commentHash);
+
+        if (posts[_id].comments.length >= 10 && posts[_id].comments.length / 10 > posts[_id].commentMilestone) {
+            posts[_id].commentMilestone = posts[_id].comments.length / 10;
+
+            // Transfer tokens to user
+            distributeReward(posts[_id].owner);
+        }
+        emit CommentCreated(_id);
+    }
+
+    function uncomment(uint _id, uint _commentId) public {
+        PostLibrary.uncomment(posts[_id], _commentId);
+    }
+
+    function distributeReward(address _ownerAddress) internal {
+        procoin.transfer(_ownerAddress, 10);
     }
 }
---
+++ contracts/libraries/PostLibrary.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+library PostLibrary {
+
+    struct Post {
+        string postImageHash;
+        string postContentHash;
+        uint id;
+
+        mapping(address => bool) hasLiked;
+        address[] likedBy;
+        uint likeMilestone;
+
+        Comment[] comments;
+        uint commentMilestone;
+        uint commentCount;
+
+        address owner;
+    }
+
+    struct Comment {
+        string commentContentHash;
+        address owner;
+        uint id;
+    }
+
+    function likePost(Post storage self) public {
+        require(!self.hasLiked[msg.sender], 'User has already liked this post!');
+        
+        self.hasLiked[msg.sender] = true;
+        self.likedBy.push(msg.sender);
+    }
+
+    function unlikePost(Post storage self) public {
+        require(self.hasLiked[msg.sender] == true, 'User has not liked this post!');
+
+        self.hasLiked[msg.sender] = false;
+        for (uint i = 0; i < self.likedBy.length; i++) {
+            if (self.likedBy[i] == msg.sender) {
+                self.likedBy[i] = self.likedBy[self.likedBy.length - 1];
+                delete self.likedBy[self.likedBy.length - 1];
+                break;
+            }
+        }
+    }
+
+    function comment(Post storage self, string memory _commentHash) public {
+        self.commentCount++;
+        Comment memory newComment = Comment(_commentHash, msg.sender, self.commentCount);
+        self.comments.push(newComment);
+    }
+
+    function uncomment(Post storage self, uint _id) public {
+        int commentIndex = _getIndexOfComment(self, _id);
+
+        require(commentIndex != -1, "This comment does not exist!");
+
+        for (uint i = uint(commentIndex); i < self.comments.length - 1; i++) {
+            self.comments[i] = self.comments[i+1];
+        }
+        self.comments.pop();
+    }
+
+    function _getIndexOfComment(Post storage self, uint _id) private view returns (int) {
+        for (uint i = 0; i < self.comments.length; i++) {
+            if (self.comments[i].id == _id) {
+                return int(i);
+            }
+        }
+        return -1;
+    }
+}

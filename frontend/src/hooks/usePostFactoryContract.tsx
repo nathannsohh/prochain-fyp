9c70716739dd42ed5d3ddde23c9f15c83612d310
Nathan Soh <nathansoh@gmail.com>
2024-01-04 12:18:25 +0800

    change address of post factory

--- frontend/src/hooks/usePostFactoryContract.tsx
+++ frontend/src/hooks/usePostFactoryContract.tsx
@@ -3,7 +3,7 @@ import { useMemo } from "react"
 import { useMetamask } from "./useMetamask"
 import ABI from "@/../../artifacts/contracts/PostFactory.sol/PostFactory.json";
 
-const address = "xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+const address = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
 
 const usePostFactoryContract = () => {
     const { state } = useMetamask();

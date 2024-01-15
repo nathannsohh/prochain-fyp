92fd65130d63ba5ce553a14154c99e27ea0904d4
Nathan Soh <nathansoh@gmail.com>
2024-01-15 22:31:09 +0800

    add initial new post card on feed

--- frontend/src/app/feed/page.tsx
+++ frontend/src/app/feed/page.tsx
@@ -1,14 +1,39 @@
 'use client'
+import NewPostCard from "@/components/feed/NewPostCard";
 import { useMetamask } from "@/hooks/useMetamask";
 import useUserManangerContract from "@/hooks/useUserFactoryContract";
 import { Contract } from "ethers";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
+import axios from 'axios'
+import { API_URL } from "@/util/constants";
+import { updateSelf } from "../profile/profileSlice";
+import { useDisclosure } from '@chakra-ui/react'
 
 const Feed = () => {
     const { state: { wallet, status } } = useMetamask();
     const router = useRouter()
     const userFactoryContract: Contract | null = useUserManangerContract();
+
+    const ownProfile: ProfileState = useAppSelector((state) => state.ownProfile)
+    const {posts, connections, ...profile}: ProfileState = ownProfile
+
+    const dispatch = useAppDispatch()
+    const { isOpen: newPostModalIsOpen, onOpen: newPostModalOnOpen, onClose: newPostModalOnClose } = useDisclosure();
+
+    const [userData, setUserData] = useState<UserType | null>(ownProfile.first_name !== null ? {
+        first_name: profile.first_name!,
+            last_name: profile.last_name!,
+            pronouns: profile.pronouns,
+            email: profile.email!,
+            wallet_address: profile.wallet_address!,
+            bio: profile.bio,
+            location: profile.location,
+            content_hash: profile.content_hash!,
+            profile_picture_hash: profile.profile_picture_hash!,
+            profile_banner_hash: profile.profile_banner_hash!
+    } : null)
 
     useEffect(() => {
         if (status === "idle") {
@@ -17,11 +42,46 @@ const Feed = () => {
                 if (!result) {
                     router.push('/profile/new')
                 }
+                if (ownProfile.first_name === null) getUserDetails()
             })
         }
     }, [wallet, status])
 
-    return <>Hello!</>
+    const getUserDetails = async () => {
+        try {
+            const [userProfile, numOfConnections] = await Promise.all([
+                userFactoryContract?.getUserProfile(wallet), 
+                userFactoryContract?.getNumberOfConnections(wallet)
+            ])
+            
+            const userResult = await axios.get(`${API_URL}/user/${userProfile.profileDataHash}`)
+            if (userResult.data.success) {
+                const user: UserType = userResult.data.user
+                const userDetails = {
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    pronouns: user.pronouns,
+                    email: user.email,
+                    wallet_address: user.wallet_address,
+                    bio: user.bio,
+                    location: user.location,
+                    content_hash: user.content_hash,
+                    profile_picture_hash: userProfile.profileImageHash,
+                    profile_banner_hash: userProfile.profileHeaderHash
+                }
+                setUserData(userDetails)
+                dispatch(updateSelf({... userDetails, connections: numOfConnections}))
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    return (
+        <>
+            <NewPostCard userData={userData} />
+        </>
+    )
 }
 
 export default Feed;
---
+++ frontend/src/components/feed/NewPostCard.tsx
@@ -0,0 +1,24 @@
+import { Card, CardBody, HStack, Avatar, VStack, Text, Box } from '@chakra-ui/react'
+
+interface NewPostCardProps {
+    userData: UserType | null
+}
+
+export default function NewPostCard(props: NewPostCardProps) {
+    return (
+        <Card width="100%" mt={2} borderRadius="20px" p={2}>
+            <CardBody>
+                <HStack>
+                    <Avatar mr={2} height="55px" width="55px" src={`http://127.0.0.1:8080/ipfs/${props.userData?.profile_picture_hash}`}/>
+                    <VStack alignItems="start" spacing={0}>
+                        <Text fontWeight="semibold" fontSize={15}>{props.userData?.first_name + ' ' + props.userData?.last_name}</Text>
+                        <Text fontSize={15} color="#7D7D7D">{props.userData?.bio}</Text>
+                    </VStack>
+                </HStack>
+                <Box width="100%" bg="#F3F3F3" mt={4} p={3} borderRadius="20px" borderWidth="1px" borderColor="#C5C1C1" cursor="pointer" _hover={{"bg": "#E9E9E9"}}>
+                    <Text ml={4} color="#4F4F4F" fontWeight="semibold">Make a new post +</Text>
+                </Box>
+            </CardBody>
+        </Card>
+    )
+}

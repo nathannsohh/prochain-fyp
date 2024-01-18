9ed483335654a49350ca7180efcc0cc795e9da4e
Nathan Soh <nathansoh@gmail.com>
2024-01-18 18:53:28 +0800

    add get posts and post owner details functionality and map it to posts

--- frontend/src/app/feed/page.tsx
+++ frontend/src/app/feed/page.tsx
@@ -7,11 +7,12 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import axios from 'axios'
-import { API_URL } from "@/util/constants";
+import { API_URL, THE_GRAPH_URL } from "@/util/constants";
 import { updateSelf } from "../profile/profileSlice";
 import { useDisclosure, useToast, Divider } from '@chakra-ui/react'
 import ProfileNewPostModal from "@/components/profile/ProfileNewPostModal";
 import Post from "@/components/feed/Post";
+import { getDetailsFromUserAddress } from "@/util/user_util";
 
 const Feed = () => {
     const { state: { wallet, status } } = useMetamask();
@@ -37,15 +38,22 @@ const Feed = () => {
             profile_picture_hash: profile.profile_picture_hash!,
             profile_banner_hash: profile.profile_banner_hash!
     } : null)
+    const [shownPosts, setShownPosts] = useState<Array<FeedPostType>>([])
 
     useEffect(() => {
         if (status === "idle") {
-            if (wallet === null) router.push('/login')
+            if (wallet === null) {
+                router.push('/login')
+                return
+            }
+            
             userFactoryContract!.doesUserExist(wallet).then((result) => {
                 if (!result) {
                     router.push('/profile/new')
+                } else {
+                    if (ownProfile.first_name === null) getUserDetails()
+                    getPosts(wallet!, 0)
                 }
-                if (ownProfile.first_name === null) getUserDetails()
             })
         }
     }, [wallet, status])
@@ -59,6 +67,22 @@ const Feed = () => {
             variant: 'subtle',
             position: 'bottom-left'
         })
+    }
+
+    const getArrayOfPostContentHashes = (postData: any): Array<string>  => {
+        let postHashArray = []
+        for (const post of postData) {
+            postHashArray.push(`"${post.postContentHash}"`)
+        }
+        return postHashArray
+    }
+
+    const getArrayOfPostOwner = (postData: any): Array<string> => {
+        let postOwnerSet = new Set<string>()
+        for (const post of postData) {
+            postOwnerSet.add(`"${post.owner}"`)
+        }
+        return Array.from(postOwnerSet)
     }
 
     const getUserDetails = async () => {
@@ -91,6 +115,37 @@ const Feed = () => {
         }
     }
 
+    const getPosts = async (user_wallet: String, page: number) => {
+        const graphqlQuery = {
+            "operationName": "getPosts",
+            "query": `query getPosts {
+                         posts (
+                            where: { postContentHash_not: ""}, 
+                            orderBy: postId,
+                            orderDirection: desc,
+                            first: 15,
+                            skip: ${15 * page}
+                        ) { owner postContentHash postImageHash id likedBy comments { commentContentHash } } }`,
+            "variables": {}
+        }
+        try {
+            const posts = await axios.post(`${THE_GRAPH_URL}/posts`, graphqlQuery)
+            const allPosts = posts.data.data.posts
+            const postHashes = getArrayOfPostContentHashes(allPosts)
+            const postOwners = getArrayOfPostOwner(allPosts)
+            const userDetails = await getDetailsFromUserAddress(postOwners)
+            console.log(userDetails)
+            const postResult = await axios.get(`${API_URL}/post/[${postHashes}]`)
+            const consolidatedPosts = allPosts.map((post: any, index: number) => {
+                return {...post, content: postResult.data.posts[index].content, time_posted: postResult.data.posts[index].time_posted, name: userDetails?.get(post.owner)?.name, bio: userDetails?.get(post.owner)?.bio, profileImageHash: userDetails?.get(post.owner)?.profileImageHash}
+            })
+            console.log(consolidatedPosts)
+            setShownPosts(prevState => prevState.concat(consolidatedPosts))
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
     return (
         <>
             <NewPostCard userData={userData} onNewPost={newPostModalOnOpen} />
@@ -103,7 +158,9 @@ const Feed = () => {
                 triggerToast={triggerToast}
                 loadUserPosts={() => {}}/>}
                 <Divider mt={3} borderColor="#C8C8C8"/>
-            <Post />
+            {shownPosts.map((post) => {
+                return <Post data={post}/>
+            })}
         </>
     )
 }
--- frontend/src/components/feed/Post.tsx
+++ frontend/src/components/feed/Post.tsx
@@ -3,7 +3,11 @@ import { BiLike } from "react-icons/bi";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { useRef, useEffect, useState } from 'react'
 
-export default function Post() {
+interface PostProps {
+    data: FeedPostType
+}
+
+export default function Post(props: PostProps) {
     const containerRef = useRef<HTMLDivElement>(null);
     const [isTruncated, setIsTruncated] = useState<Boolean>(false);
     const [isSeeMore, setIsSeeMore] = useState<Boolean>(false)
@@ -20,32 +24,49 @@ export default function Post() {
     const seeMoreHandler = () => {
         setIsSeeMore(prevState => !prevState)
     }
+
+    const getTimeDifference = (dateString: string): string => {
+        const currentDate = new Date()
+        const targetDate = new Date(dateString)
+
+        const timeDifference = currentDate.getTime() - targetDate.getTime()
+        const dayDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        if (dayDifference === 0) {
+            return "Today"
+        } else if (dayDifference < 30) {
+            return `${dayDifference}d ago`
+        } else if (dayDifference < 365){
+            const monthDifference = Math.floor(dayDifference / 30)
+            return `${monthDifference}m ago`
+        } else {
+            const yearDifference = Math.floor(dayDifference / 365)
+            return `${yearDifference}y ago`
+        }
+    }
     
     return (
-        <Card width="100%" mt={2} borderRadius="20px" pt={2} pb={1}>
+        <Card width="100%" mt={2} borderRadius="20px" pt={0} pb={1}>
             <CardBody pl={0} pr={0} pb={1}>
                 <Box pl={7} pr={7} mb={3}>
                     <HStack>
-                        <Avatar mr={2} height="55px" width="55px"/>
+                        <Avatar mr={2} height="55px" width="55px" src={`http://127.0.0.1:8080/ipfs/${props.data.profileImageHash}`}/>
                         <VStack alignItems="start" spacing={0}>
-                            <Text fontWeight="semibold" fontSize={14} height={5}>Nathan Soh</Text>
-                            <Text fontSize={13} color="#7D7D7D" height={5}>Final Year Computer Science Student at Nanyang Technological University</Text>
-                            <Text fontSize={13} color="#7D7D7D" height={5}>5d ago</Text>
+                            <Text fontWeight="semibold" fontSize={14} height={5}>{props.data.name}</Text>
+                            <Text fontSize={13} color="#7D7D7D" height={5}>{props.data.bio}</Text>
+                            <Text fontSize={13} color="#7D7D7D" height={5}>{getTimeDifference(props.data.time_posted)}</Text>
                         </VStack>
                     </HStack>
                     <Text mt={3} noOfLines={isSeeMore ? undefined : 3} ref={containerRef}>
-                        Top 4 reasons why data engineering is the best data profession:
-
-                        1. highest pay for the least education
-                        Machine learning engineers and data scientists make 10-15% more but spent 30% more time in college. Data analysts make less than data engineers but require less schooling.
+                        {props.data.content}
                     </Text>
                     {isTruncated && !isSeeMore && <Button variant="link" onClick={seeMoreHandler}>See more...</Button>}
                     {isTruncated && isSeeMore && <Button variant="link" onClick={seeMoreHandler}>See less...</Button>}
                 </Box>
                 <Divider width="100%"/>
                 <HStack pl={7} pr={7} mt={3}>
-                    <Button leftIcon={<BiLike size={20} />} variant="ghost">13</Button>
-                    <Button leftIcon={<FaRegCommentAlt size={20} />} variant='ghost'>14</Button>
+                    <Button leftIcon={<BiLike size={20} />} variant="ghost">{props.data.likedBy.length}</Button>
+                    <Button leftIcon={<FaRegCommentAlt size={20} />} variant='ghost'>{props.data.comments.length}</Button>
                     <Spacer />
                 </HStack>
             </CardBody>

a9ce0e7cc561a350ee7c044b6387a2ed83ecf7ee
Nathan Soh <nathansoh@gmail.com>
2024-01-25 11:23:56 +0800

    add hovering to navigate to profile

--- frontend/src/components/network/Connection.tsx
+++ frontend/src/components/network/Connection.tsx
@@ -1,6 +1,8 @@
 import { Box, Avatar, HStack, VStack, Text, Divider, Spacer, IconButton, MenuButton, MenuList, MenuItem, Menu } from "@chakra-ui/react";
+import { useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
 import { FaTrash } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 interface ConnectionProps {
     user: UserDetails,
@@ -8,14 +10,31 @@ interface ConnectionProps {
 }
 
 export default function Connection(props: ConnectionProps) {
+    const [isHovering, setIsHovering] = useState<boolean>(false)
+    const router = useRouter()
+
+    const onMouseEnterHandler = () => {
+        setIsHovering(true)
+    }
+
+    const onMouseLeaveHandler = () => {
+        setIsHovering(false)
+    }
+
+    const nameOnClickHandler = () => {
+        router.push(`/profile/${props.user.address}`)
+    }
+
     return (
         <Box>
             <HStack p={7} pt={3} pb={3}>
-                <Avatar mr={2} src={`http://127.0.0.1:8080/ipfs/${props.user.profileImageHash}`}/>
-                <VStack alignItems="start" spacing={0}>
-                    <Text fontWeight="semibold" fontSize={14} height={5}>{props.user.name}</Text>
-                    <Text fontSize={13} color="#7D7D7D" height={5}>{props.user.bio}</Text>
-                </VStack>
+                <HStack onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} onClick={nameOnClickHandler} _hover={{cursor: "pointer"}}>
+                    <Avatar mr={2} src={`http://127.0.0.1:8080/ipfs/${props.user.profileImageHash}`}/>
+                    <VStack alignItems="start" spacing={0}>
+                        <Text fontWeight="semibold" fontSize={14} height={5} as={isHovering ? "u" : undefined}>{props.user.name}</Text>
+                        <Text fontSize={13} color="#7D7D7D" height={5} as={isHovering ? "u" : undefined}>{props.user.bio}</Text>
+                    </VStack>
+                </HStack>
                 <Spacer />
                 <Menu>
                     <MenuButton as={IconButton} aria-label={"options"} icon={<BsThreeDots />} isRound variant="ghost"/>
--- frontend/src/components/network/ConnectionRequest.tsx
+++ frontend/src/components/network/ConnectionRequest.tsx
@@ -1,4 +1,5 @@
 import { Box, Avatar, HStack, VStack, Text, Divider, Spacer, Button } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 interface ConnectionRequestProps {
@@ -10,6 +11,7 @@ interface ConnectionRequestProps {
 export default function ConnectionRequest(props: ConnectionRequestProps) {
     const [acceptLoading, setAcceptLoading] = useState<boolean>(false)
     const [ignoreLoading, setIgnoreLoading] = useState<boolean>(false)
+    const [isHovering, setIsHovering] = useState<boolean>(false)
 
     const acceptRequest = async () => {
         try {
@@ -33,14 +35,30 @@ export default function ConnectionRequest(props: ConnectionRequestProps) {
         }
     }
 
+    const router = useRouter()
+
+    const onMouseEnterHandler = () => {
+        setIsHovering(true)
+    }
+
+    const onMouseLeaveHandler = () => {
+        setIsHovering(false)
+    }
+
+    const nameOnClickHandler = () => {
+        router.push(`/profile/${props.user.address}`)
+    }
+
     return (
         <Box>
             <HStack p={7} pt={3} pb={3}>
-                <Avatar mr={2} src={`http://127.0.0.1:8080/ipfs/${props.user.profileImageHash}`}/>
-                <VStack alignItems="start" spacing={0}>
-                    <Text fontWeight="semibold" fontSize={14} height={5}>{props.user.name}</Text>
-                    <Text fontSize={13} color="#7D7D7D" height={5}>{props.user.bio}</Text>
-                </VStack>
+                <HStack onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} onClick={nameOnClickHandler} _hover={{cursor: "pointer"}}>
+                    <Avatar mr={2} src={`http://127.0.0.1:8080/ipfs/${props.user.profileImageHash}`}/>
+                    <VStack alignItems="start" spacing={0}>
+                        <Text fontWeight="semibold" fontSize={14} height={5} as={isHovering ? "u" : undefined}>{props.user.name}</Text>
+                        <Text fontSize={13} color="#7D7D7D" height={5} as={isHovering ? "u" : undefined}>{props.user.bio}</Text>
+                    </VStack>
+                </HStack>
                 <Spacer />
                 <Button variant="ghost" borderRadius="20px" onClick={ignoreRequest} isLoading={ignoreLoading}>Ignore</Button>
                 <Button colorScheme="blue" borderRadius="20px" onClick={acceptRequest} isLoading={acceptLoading}>Accept</Button>

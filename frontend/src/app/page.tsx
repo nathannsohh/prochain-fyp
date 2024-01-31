0801fbf658c5e470f76fe94c1c0e2c116cd2fddd
Nathan Soh <nathansoh@gmail.com>
2024-01-31 22:50:16 +0800

    remove unneeded code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,54 +8,16 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const {
-    dispatch,
-    state: { status, wallet },
-  } = useMetamask();
-  const listen = useListen();
+  const { state: { status, wallet } } = useMetamask();
   const router = useRouter();
   
   useEffect(() => {
-    if (status === "idle") {
-      if (wallet == null) {
-        router.push('/login')
-      } else {
-        router.push('/feed')
-      }
+    if (status === "idle" && wallet != null) {
+        router.push('/feed')  
     }
   }, [wallet, status]);
 
-
-
-// const showInstallMetamask = status !== "pageNotLoaded" && !isMetamaskInstalled;
-// const showConnectButton = status !== "pageNotLoaded" && isMetamaskInstalled && !wallet;
-
-// const isConnected = status !== "pageNotLoaded" && typeof wallet === "string";
-
-// const handleConnect = async () => {
-//     dispatch({ type: "loading" });
-//     const accounts = await window.ethereum.request({
-//         method: "eth_requestAccounts",
-//     });
-
-//     if (accounts.length > 0) {
-//         const balance = await window.ethereum!.request({
-//             method: "eth_getBalance",
-//             params: [accounts[0], "latest"],
-//         });
-//         dispatch({ type: "connect", wallet: accounts[0], balance });
-
-//         // we can register an event listener for changes to the users wallet
-//         listen();
-//     }
-// }
-
-const handleDisconnect = () => {
-    dispatch({ type: "disconnect" });
-  };
-
   return (
-    <main className={styles.main}>
-    </main>
+    <main className={styles.main} />
   )
 }

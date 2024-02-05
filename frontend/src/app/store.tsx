1a2bd8210c75c878d1c931adef3febcc2017a8bf
Nathan Soh <nathansoh@gmail.com>
2024-02-05 13:21:07 +0800

    add org profile reducer for redux

--- frontend/src/app/store.tsx
+++ frontend/src/app/store.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import profileReducer from '../redux_slices/profileSlice'
 import profileTypeReducer from '../redux_slices/profileTypeSlice'
+import orgProfileReducer from '../redux_slices/organisationProfileSlice'
 
 export const store = configureStore({
   reducer: {
     ownProfile: profileReducer,
-    profileType: profileTypeReducer
+    profileType: profileTypeReducer,
+    orgProfile: orgProfileReducer
   },
 })
 

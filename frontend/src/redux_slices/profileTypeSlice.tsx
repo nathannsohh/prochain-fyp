0c38bff43dbdaaaeaaa5f87d91a561175157dd0b
Nathan Soh <nathansoh@gmail.com>
2024-02-24 20:03:08 +0800

    change initial state to avoid incorrect renders

--- frontend/src/redux_slices/profileTypeSlice.tsx
+++ frontend/src/redux_slices/profileTypeSlice.tsx
@@ -2,7 +2,7 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export const profileTypeSlice = createSlice({
     name: 'profileType',
-    initialState: 1,
+    initialState: -1,
     reducers: {
         updateType: (state, action: PayloadAction<number>) => {
             if (action.payload === undefined) {

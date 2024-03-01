5ec71bf01d910cf242d8631e6875df450c16fc68
Nathan Soh <nathansoh@gmail.com>
2024-03-01 12:22:27 +0800

    update constants to have job values

--- frontend/src/util/constants.tsx
+++ frontend/src/util/constants.tsx
@@ -284,6 +284,20 @@ export const industries: string[] = [
 	"Wellness & Fitness"
 ]
 
+export const employmentTypeList = [
+	"Full-Time",
+	"Part-Time",
+	"Internship",
+	"Contract"
+]
+
+export const jobLevelList = [
+	"No Experience Required",
+	"Entry-Level",
+	"Mid-Level",
+	"Senior-Level"
+]
+
 export const API_URL: string = "http://localhost:8002"
 export const THE_GRAPH_URL: string = "http://127.0.0.1:8000/subgraphs/name"
 export const IPFS_URL: string = "http://127.0.0.1:5001/api/v0"
